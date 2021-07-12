6dc9b93f459a88cc0bc2c8461bc50f9c39d1725a
Parada <paradafaii28@gmail.com>
2021-07-11 20:15:21 -0400

    adding styling to videoItem component with sementic ui

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -5,7 +5,7 @@ const VideoList = ({videos}) => {
     const renderedList = videos.map((video, idx) => {
         return <VideoItem key={idx} video={video}/>
     });
-    return <div>{renderedList}</div>;
+    return <div className="ui relaxed divided list">{renderedList}</div>;
 }
 
 export default VideoList
