20a5f090b0107d9633f51d865d39aabe374e36ba
Parada <paradafaii28@gmail.com>
2021-07-11 20:28:52 -0400

    write callback function name onVideoSelect and passing to videolist and videoItem, using onclick and passing video with onVideoSelect callback fucntion

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,28 @@ import youtube from '../apis/youtube'
 
 class App extends React.Component {
     state = {
-      videos: []
+      videos: [],
+      selectedVideo: null
     };
-     onTermSubmit = async term => {
-        const response = await youtube.get('./search', {
-          params: {
-            q: term
-          }
-        });
+    onTermSubmit = async term => {
+      const response = await youtube.get('./search', {
+        params: {
+          q: term
+        }
+      });
 
-        this.setState({ videos: response.data.items})
+      this.setState({ videos: response.data.items})
+    }
+
+    onVideoSelect = (video) => {
+      console.log('Form the App', video)
     }
 
     render() {
         return (
           <div className="ui container">
             <SearchBar callParentSubmit={this.onTermSubmit} />
-            <VideoList videos={this.state.videos}/>
+            <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
           </div>
         );
     }
--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import VideoItem from "./videoItem";
 
-const VideoList = ({videos}) => {
-    const renderedList = videos.map((video, idx) => {
-        return <VideoItem key={idx} video={video}/>
-    });
-    return <div className="ui relaxed divided list">{renderedList}</div>;
-}
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video, idx) => {
+    return <VideoItem key={idx} video={video} onVideoSelect={onVideoSelect} />;
+  });
+  return <div className="ui relaxed divided list">{renderedList}</div>;
+};
 
 export default VideoList
